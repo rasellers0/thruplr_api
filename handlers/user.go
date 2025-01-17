3ba41bf3919fe576c37a9e28722eb72f717c0f0e
rasellers0 <rasellers0@gmail.com>
2025-01-16 19:48:54 -0600

    updates to user.go
    adding in a basic function to retrieve user data by user id

--- handlers/user.go
+++ handlers/user.go
@@ -1,17 +1,34 @@
 package handlers
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
-	util "thruplr_api/util"
 
 	"github.com/labstack/echo/v4"
 )
+
+type UserData struct {
+	UserID      string
+	AccountType string
+	FirstName   string
+	MiddleName  string
+	LastName    string
+	DOB         string
+	Location    string
+	Pronouns    string
+	Orientation string
+	Race1       string
+	Race2       string
+	Ethnicity   string
+	Religion    string
+	Politics    string
+	Family      string
+	FamilyPlans string
+	LookingFor  string
+	AboutMe     string
+}
 
 func Hello(c echo.Context) error {
 	fmt.Println("so it'll hit here, ok")
@@ -19,26 +36,41 @@ func Hello(c echo.Context) error {
 }
 
 func GetUserData(c echo.Context) error {
-	jsonFile, err := os.Open("./json/user.json")
-	if err != nil {
-		log.Println(err)
-		return c.String(http.StatusOK, "No User Data")
-	}
-	log.Println("Successfully Opened users.json")
-	defer jsonFile.Close()
+	userQuery := `select u.user_id, u.first_name, u.middle_name, u.last_name, u.account_type,
+	fd.pronouns, fd.orientation, fd.dob, ifnull(fd.race1, '') as race1, ifnull(fd.race2, '') as race2, 
+	ifnull(fd.ethnicity, '') as ethnicity, fd.location, ifnull(usd.about_me, '') as about_me, 
+	ifnull(usd.religion, '') as religion, ifnull(usd.politics, '') as politics, ifnull(usd.family, '') as family,
+	ifnull(usd.family_plans, '') as family_plans, ifnull(usd.looking_for, '') as looking_for
+	from users u
+	left join user_demographics fd on fd.user_id = u.user_id
+	left join user_details usd on u.user_id = fd.user_id
+	where u.user_id = ?`
 
-	byteValue, _ := io.ReadAll(jsonFile)
-	user := new(util.User)
-	err = json.Unmarshal(byteValue, &user)
-	if err != nil {
-		log.Println(err.Error())
-		return c.String(http.StatusOK, "No User Data")
-	} else {
-		log.Println("no errors found")
-	}
-	log.Println("User id: " + strconv.Itoa(int(user.ID)))
-	log.Println("User First Name: " + user.FirstName)
-	log.Println("User Last Name: " + user.LastName)
+	userid := c.Param("id")
 
-	return c.JSON(http.StatusOK, user)
+	var (
+		user_values UserData
+	)
+
+	db, err := sql.Open("mysql", "root:NovemberKazoo01!@tcp(127.0.0.1:3306)/thruplr")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := db.Query(userQuery, userid)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	if stmt.Next() {
+		err := stmt.Scan(&user_values.UserID, &user_values.FirstName, &user_values.MiddleName, &user_values.LastName,
+			&user_values.AccountType, &user_values.Pronouns, &user_values.Orientation, &user_values.DOB, &user_values.Race1,
+			&user_values.Race2, &user_values.Ethnicity, &user_values.Location, &user_values.AboutMe, &user_values.Religion,
+			&user_values.Politics, &user_values.Family, &user_values.FamilyPlans, &user_values.LookingFor)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return c.JSON(http.StatusOK, user_values)
 }
